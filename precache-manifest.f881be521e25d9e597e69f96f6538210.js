4c059b6a8f91401aebb2de6d23164af9407c98d8
Agbor Ojen Jr <ojenagborjnr@gmail.com>
2020-07-05 01:26:22 +0100

    Updates

---
+++ precache-manifest.f881be521e25d9e597e69f96f6538210.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a6ec5f3bf1c9bbb53c7bdc4e6c2cf80",
+    "url": "/facerecognition/index.html"
+  },
+  {
+    "revision": "43be5e8864d73c5d88cd",
+    "url": "/facerecognition/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b68f8678f7353893cb73",
+    "url": "/facerecognition/static/js/2.5a1a9b07.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/facerecognition/static/js/2.5a1a9b07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43be5e8864d73c5d88cd",
+    "url": "/facerecognition/static/js/main.99e3cf1b.chunk.js"
+  },
+  {
+    "revision": "fa6a11a9cd7eebd80802",
+    "url": "/facerecognition/static/js/runtime-main.1ad3eaac.js"
+  }
+]);
