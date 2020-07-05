262bf8b77f24443216a2b431f2ec066a1126b3eb
Agbor Ojen Jr <ojenagborjnr@gmail.com>
2020-07-05 01:53:13 +0100

    Added gradient and Tachyons

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import 'tachyons';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -12,6 +13,6 @@ ReactDOM.render(
 );
 
 // If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
+// unregister() to register () below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
