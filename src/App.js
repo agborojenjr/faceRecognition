de69a658e84c1258913e6e485e93cb1315da492d
Agbor Ojen Jr <ojenagborjnr@gmail.com>
2020-07-12 09:16:57 +0100

    Introduced Clarifai API

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
 import Navigation from './components/Navigation/Navigation';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 
 import './App.css';
 
+const app = new Clarifai.App({
+ apiKey: '7c36ebe69f2643989d553dee0dea0eac'
+});
+ 
 const particlesOptions = {
   particles: {
        number: {
@@ -16,23 +22,53 @@ const particlesOptions = {
           value_area: 800
         }
       }          
+  }  
+}
+
+class App extends Component { 
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+    }
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target.value);
+  }
+
+  onButtonSubmit = () => {
+    console.log('click');
+    app.models.predict(
+      "a403429f2ddf4b49b307e318f00e528b", 
+      "https://samples.clarifai.com/face-det.jpg")
+    .then(
+      function(response) {
+       console.log(response);
+      },
+      function(err) {
+        // there was an error
+      }
+    );
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Particles className='particles'
+                params={particlesOptions}
+        />
+         <Navigation />
+         <Logo />
+         <Rank />
+          <ImageLinkForm 
+            onInputChange={this.onInputChange} 
+            onButtonSubmit={this.onButtonSubmit}
+          />
+        <FaceRecognition />
+      </div>
+    );
   }
 }
-
-function App() { 
-  return (
-    <div className="App">
-    <Particles className='particles'
-              params={particlesOptions}
-            />
-      <Navigation />
-      <Logo />
-      <Rank />
-      <ImageLinkForm />
-            {/*
-            <FaceRecognition />*/}
-    </div>
-  );
-}
-
+   
 export default App;
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -4,7 +4,7 @@ const Navigation = () => {
 	return (
 		<nav style={{display: 'flex', justifyContent: 'flex-end'}}>
 			<p className='f3 link dim black underline pa3 pointer'>Sign Out</p>
-		</nav>
+		</nav>  
 	);
 }
 
